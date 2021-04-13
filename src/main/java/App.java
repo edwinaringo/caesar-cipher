9e3884ebd3eb68f048c21d1620f1f51e6acb9772
edwinaringo <tedaringo628@gmail.com>
2021-04-13 23:55:11 +0300

    Modifying the user logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,13 +5,13 @@ public class App {
     public static void main(String[] args) {
         System.out.println("WELCOME TO THE CAESAR CIPHER");
         while (true) {
-            System.out.print("CHOOSE e for ENCRYPTION ");
+            System.out.println("CHOOSE e for ENCRYPTION ");
             Scanner input = new Scanner(System.in);
             String choice = input.nextLine().toLowerCase();
             if (choice.equals("e")) {
-                System.out.print("Enter text to be encrypted: ");
+                System.out.println("Enter text to be encrypted: ");
                 String plainText = input.nextLine().toLowerCase();
-                System.out.print("Enter the shift value: ");
+                System.out.println("Enter the shift value: ");
                 int shiftValue = input.nextInt();
                 EncryptDecrypt message = new EncryptDecrypt(plainText, shiftValue);
                 System.out.println("Plain Text: " + message.getTextMessage() + "\n Shift Value of: " + message.getShiftBy());
