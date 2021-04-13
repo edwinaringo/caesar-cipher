ffbb8570809b9cb98815d9651ca32c5cce49f522
edwinaringo <tedaringo628@gmail.com>
2021-04-13 23:38:13 +0300

    Adding logic to tests

--- src/main/java/EncryptDecrypt.java
+++ src/main/java/EncryptDecrypt.java
@@ -1,2 +1,21 @@
 public class EncryptDecrypt {
+    private String textMessage;
+    private int shiftBy;
+
+    public EncryptDecrypt(String textMessage, int shiftBy) {
+        this.textMessage = textMessage;
+        this.shiftBy = shiftBy;
+    }
+
+    public String getTextMessage() {
+        return null;
+
+    }
+
+    public Object getShiftBy() {
+        return null;
+    }
+
+
+
 }
--- src/test/java/EncryptDecryptTest.java
+++ src/test/java/EncryptDecryptTest.java
@@ -1,5 +1,20 @@
-import static org.junit.Assert.*;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class EncryptDecryptTest {
+    @Test
+    public void encryptDecrypt_savesMessage_String() {
+        EncryptDecrypt testVal = new EncryptDecrypt("hello",3);
+        assertEquals("hello", testVal.getTextMessage());
+    }
+
+    @Test
+    public void encryptDecrypt_savesShiftValue_Integer() {
+        EncryptDecrypt testVal = new EncryptDecrypt("hello",3);
+        assertEquals(3, testVal.getShiftBy());
+    }
+
 
 }
